4e7c083bfc02bcd432f0a6d62a9a089f5830f706
Juan Urdaneta <juandurdanet@gmail.com>
2021-09-02 19:12:39 -0400

    Added account details

--- scripts/auth.js
+++ scripts/auth.js
@@ -5,6 +5,31 @@ const register_modal = new mdb.Modal(modalRegister)
 const modalLogin = document.getElementById('loginModal');
 const login_modal = new mdb.Modal(modalLogin);
 
+
+// listen for auth status
+auth.onAuthStateChanged(user =>{
+    if(user){
+        document.getElementById('logout').classList.remove("d-none");
+        document.getElementById('account').classList.remove("d-none");
+        document.getElementById('login').classList.add("d-none");
+        document.getElementById('signup').classList.add("d-none");
+
+        // account details
+
+        document.getElementById('accountUsername').innerHTML = "Username: "+ user.displayName
+        document.getElementById('accountMail').innerHTML = "Mail: "+ user.email;
+        document.getElementById('accountDate').innerHTML = "Date of registration: "+ user.metadata.creationTime;
+
+
+
+    } else {
+        document.getElementById('login').classList.remove("d-none");
+        document.getElementById('signup').classList.remove("d-none");
+        document.getElementById('logout').classList.add("d-none");
+        document.getElementById('account').classList.add("d-none");
+    }
+})
+
 // sign up 
 signUpForm.addEventListener('submit', (e)=>{
     e.preventDefault();
@@ -19,8 +44,12 @@ signUpForm.addEventListener('submit', (e)=>{
     // checking for matching passwords
     if(password === confirmPassword){
         auth.createUserWithEmailAndPassword(email, password).then(cred =>{
+            cred.user.updateProfile({
+                displayName: signUpForm['name'].value
+            })
             register_modal.toggle();
-            signUpForm.reset();   
+            signUpForm.reset();
+
         })
 
     // catching errors
@@ -51,9 +80,7 @@ signUpForm.addEventListener('submit', (e)=>{
 const logout = document.querySelector('#logout');
 logout.addEventListener('click', (e) =>{
     e.preventDefault();
-    auth.signOut().then(()=>{
-        console.log("logged out succesfully!");
-    })
+    auth.signOut();
 })
 
 // log in
@@ -67,10 +94,12 @@ loginForm.addEventListener('submit', (e)=>{
     const password = loginForm['password'].value;
 
     auth.signInWithEmailAndPassword(email, password).then(cred =>{
-        console.log(cred.user);
 
         // closing the modal and resetting the form
         login_modal.toggle();
         loginForm.reset();
     })
 })
+
+// account details
+
