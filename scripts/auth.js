fbb659659286d8787e13651543259607d7f907ae
Juan Urdaneta <juandurdanet@gmail.com>
2021-09-02 17:54:15 -0400

    Adding user login

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,4 +1,11 @@
 const signUpForm = document.querySelector("#signUpForm");
+const modalRegister = document.getElementById('signUpModal')
+const register_modal = new mdb.Modal(modalRegister)
+
+const modalLogin = document.getElementById('loginModal');
+const login_modal = new mdb.Modal(modalLogin);
+
+// sign up 
 signUpForm.addEventListener('submit', (e)=>{
     e.preventDefault();
 
@@ -12,12 +19,13 @@ signUpForm.addEventListener('submit', (e)=>{
     // checking for matching passwords
     if(password === confirmPassword){
         auth.createUserWithEmailAndPassword(email, password).then(cred =>{
-            console.log(cred);
+            register_modal.toggle();
+            signUpForm.reset();   
         })
 
+    // catching errors
         .catch((error) =>{
             const error_code = error.code;
-            console.log(error_code);
             switch (error_code) {
                 case "auth/weak-password":
                     signUpForm['password'].classList.add('is-invalid');
@@ -31,8 +39,38 @@ signUpForm.addEventListener('submit', (e)=>{
             }
 
         }) 
+    // in case posswords dont match
     } else {
         signUpForm['confirm-password'].classList.add('is-invalid')
     }
 
+});
+
+// log out
+
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', (e) =>{
+    e.preventDefault();
+    auth.signOut().then(()=>{
+        console.log("logged out succesfully!");
+    })
+})
+
+// log in
+
+const loginForm = document.querySelector("#loginForm");
+loginForm.addEventListener('submit', (e)=>{
+    e.preventDefault();
+
+    // getting user info
+    const email = loginForm['email'].value;
+    const password = loginForm['password'].value;
+
+    auth.signInWithEmailAndPassword(email, password).then(cred =>{
+        console.log(cred.user);
+
+        // closing the modal and resetting the form
+        login_modal.toggle();
+        loginForm.reset();
+    })
 })
